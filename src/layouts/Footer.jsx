c117cf6b34b582c7bc2336c25276d21c9f9f3db0
Marcomussa <marcomussa567@gmail.com>
2024-10-12 23:08:28 -0400

    Fix icons footer icons bug

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,31 +1,42 @@
-import '../styles/Nav.css'
-import Logo from '../assets/img/logo.png'
-import { FaFacebookF, FaInstagram } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import "../styles/Nav.css";
+import Logo from "../assets/img/logo.png";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      <div className='container bg-black'>
-        <div className='row'>
+      <div className="container bg-black">
+        <div className="row">
           <div className="mt-5 text-center col-md-12">
-            <Link>
-              <img src={FaFacebookF} width={34} alt="Facebook" className='m-2'/>
-              <img src={FaInstagram} width={34} alt="Instagram" className='m-2' />
+            <Link to="#">
+              {/* Ícono de Facebook */}
+              <FaFacebookF size={34} className="m-2" style={{color: 'white'}}/>
+            </Link>
+            <Link to="#">
+              {/* Ícono de Instagram */}
+              <FaInstagram size={34} className="m-2" style={{color: 'white'}}/>
             </Link>
           </div>
           <div className="col-md-12 text-center mt-2 mb-3">
             <img src={Logo} alt="Logo" width={100} />
           </div>
           <div className="col-md-12 px-4">
-            <p className='text secondari text-center'>REVLON© 2024. Todos los Derechos Reservados.
-              Al registrar tu compra en esta pagina, aceptas que has leido y entendido los terminos y condiciones.
-              La participacion esta sujeta a la verificacion de la validez del ticket y el cumplimiento de los requisitos establecidos. Verifica las Bases Legales para saber conocer los lineamientos legales que rigen nuestra dinamica.
+            <p className="text secondari text-center">
+              REVLON© 2024. Todos los Derechos Reservados. Al registrar tu
+              compra en esta pagina, aceptas que has leido y entendido los
+              terminos y condiciones. La participacion esta sujeta a la
+              verificacion de la validez del ticket y el cumplimiento de los
+              requisitos establecidos. Verifica las Bases Legales para saber
+              conocer los lineamientos legales que rigen nuestra dinamica.
             </p>
           </div>
           <div className="col-md-12 px-4 mb-5">
-            <p className='text text-center text-white subtitle px-4 mb-5 '> Este sorteo es exclusivo para mayores de 18 años.
-              Los datos personales proporcionados seran tratados de cauerdo con nuestra Politica de Privacidad.
+            <p className="text text-center text-white subtitle px-4 mb-5 ">
+              {" "}
+              Este sorteo es exclusivo para mayores de 18 años. Los datos
+              personales proporcionados seran tratados de cauerdo con nuestra
+              Politica de Privacidad.
             </p>
           </div>
         </div>
