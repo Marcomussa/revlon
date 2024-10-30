5793916d607b56934c5c3c4aac27dd2f95bb477a
NicoVeron <nico.veron09@gmail.com>
2024-10-30 16:44:18 -0300

    Change NotFound Button

--- src/assets/components/Winner.jsx
+++ src/assets/components/Winner.jsx
@@ -11,8 +11,8 @@ const Winner = () => {
           </div>
           <div className="col-12 col-md-8 col-xl-8 text-center text-xl-start pe-xl-5">
             <h2 className='text-white primary-font title mt-4 mt-xl-5 '>¡FELICIDADES A LOS GANADORES DE ESTA SEMANA!</h2>
-            <div className='bg-white rounded-3 text-xl-start'>
-              <p className='fs-5 mt-4 px-3 py-2'>MARÍA FERNÁNDA FERNANDEZ DE CÓRDOVA</p>
+            <div className='text-xl-start d-flex'>
+              <p className='fs-5 mt-4 px-3 py-2 bg-white rounded-3 p-xl-3'>MARÍA FERNÁNDA FERNANDEZ DE CÓRDOVA</p>
             </div>
             <p className='primary-font subtitle text-white mb-5'>1 KIT Especial Party Revlon</p>
           </div>
